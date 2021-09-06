7cae14a025ffd99b63f8eb08de846404a327d941
Nicola Milella <nicombk@gmail.com>
2021-09-06 13:28:29 +0200

    auth con fortify

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row">
+        <div class="col-12 col-md-6 offset-md-3">
+            <form action="{{route('register')}}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                  </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password Confirmation</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password_confirmation">
+                  </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+              </form>
+        </div>
+    </div>
+@endsection
